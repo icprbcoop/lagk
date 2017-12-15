c55aaffd17f54957cec7e27f8536fd12ba79f6ac
cheriels <cschultz@icprb.org>
2017-12-15 16:53:33 -0500

    added losses; added k_lag_reaches.csv and k_lag_subreaches.csv; renamed some variables

--- functions/variable_lagk_func_test.R
+++ functions/variable_lagk_func_test.R
@@ -1,40 +1,41 @@
 
 variable_lagk <- function(long.df, site.name, por.lag, klag.df) {
-  #------------------------------------------------------------------------------
-  # To reduce number of parameters to be adjusted, focus on lag_to_lfalls
-  #    and loss_to_lfalls
+  # long.df is name of time series hourly flow data file
+  # site.name is name of node (e.g. gage) of hydrograph to be lagged (from time series hourly flow data file)
+  # por.lag is name of the subreach for which the lag/loss applies
+  # klag.df is the table giving subreach, subreach_lag, subreach_loss
+  #
   klag.sub <- klag.df %>% 
-    dplyr::filter(site == por.lag) %>% 
+    dplyr::filter(subreach == por.lag) %>% 
 #    dplyr::select(flow, lag)
-    dplyr::mutate(lag = lag_to_lfalls*pct_of_stream_length,
-                  loss = loss_to_lfalls*pct_of_stream_length) %>%
-    dplyr::select(flow, lag, loss)
+    dplyr::mutate(lag = subreach_lag, loss = subreach_loss, flow_bin = flow_cfs) %>% 
+    dplyr::select(flow_bin, lag, loss)
 
   #------------------------------------------------------------------------------
   predicted.df <- long.df %>% 
     dplyr::filter(site == site.name) %>% 
-    dplyr::mutate(range = as.character(cut(flow, breaks = klag.sub$flow)),
+    dplyr::mutate(range = as.character(cut(flow, breaks = klag.sub$flow_bin)),
                   lower_flow = as.numeric(gsub("\\(|,.*", "", range)),
                   upper_flow = as.numeric(gsub(".*,|\\]", "", range))) %>% 
-    dplyr::left_join(klag.sub, by = c("lower_flow" = "flow")) %>% 
-    dplyr::left_join(klag.sub, by = c("upper_flow" = "flow")) %>% 
+    dplyr::left_join(klag.sub, by = c("lower_flow" = "flow_bin")) %>% # changed "flow" to "flow_bin" here?
+    dplyr::left_join(klag.sub, by = c("upper_flow" = "flow_bin")) %>% 
     dplyr::rename(lower_lag = lag.x,
                   lower_loss = loss.x,
                   upper_lag = lag.y,
                   upper_loss = loss.y) %>% 
     dplyr::mutate(lag = lower_lag + ((upper_lag - lower_lag) * (flow - lower_flow) / (upper_flow - lower_flow)),
-                  arrival_time = date_time + lag * 3600) %>% 
+                  date_time_lagged = date_time + lag * 3600) %>% 
     dplyr::mutate(loss = lower_loss + ((upper_loss - lower_loss) * (flow - lower_flow) / (upper_flow - lower_flow)),
                   flow2 = flow*(1 - loss)) %>% 
   # Grabbing a lot right now for QA'ing purposes:
-    dplyr::select(date_time, arrival_time, site, lower_flow, upper_flow, flow, lag, loss, flow2) %>% 
-    dplyr::rename(date_time = lag) %>% 
+  #  dplyr::select(date_time, date_time_lagged, site, lower_flow, upper_flow, flow, lag, loss, flow2) %>% 
+    dplyr::select(date_time_lagged, site, flow2) %>%
+    dplyr::rename(date_time = date_time_lagged) %>% 
     dplyr::mutate(site = "predicted") %>% 
     dplyr::mutate(date_time = as.POSIXct(round(date_time, units = "hours"))) %>% 
     dplyr::group_by(date_time, site) %>% 
-    dplyr::summarise(flow = mean(flow)) %>%
-  # CS: apply loss:
-  #  dplyr::mutate(flow = flow*)
+    dplyr::summarise(flow = mean(flow2))
+  # 
   #------------------------------------------------------------------------------
   start.date <- min(predicted.df$date_time, na.rm = TRUE)
   end.date <- max(predicted.df$date_time, na.rm = TRUE)
